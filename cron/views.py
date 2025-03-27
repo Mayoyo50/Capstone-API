aa428af12f6c441f35f431dd93bd4e43ebfcdf68
Franklin Mayoyo <franklinmonda70@gmail.com>
2025-03-27 16:06:53 +0300

    Fix cron job 401 Unauthorized

--- cron/views.py
+++ cron/views.py
@@ -1,21 +1,12 @@
+from django.views import View
+from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.views import APIView
-from rest_framework.response import Response
-import logging
-
-logger = logging.getLogger(__name__)
 
 @method_decorator(csrf_exempt, name='dispatch')
-class HealthCheckView(APIView):
-    """
-    Simple health check endpoint.
-    """
-    
+class HealthCheckView(View):
     def get(self, request, *args, **kwargs):
-        logger.info("Health check endpoint was hit - service is alive")
-        return Response({
+        return JsonResponse({
             "status": "success",
             "message": "Service is running",
-            "service": "Capstone API", 
         })
